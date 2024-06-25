6fcbe86a6d37d46784cb7ea6b42df4ca2693d327
Srithin A V <srithin@Srithins-MacBook-Air.local>
2024-06-25 17:38:31 +0530

    added limiter and cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import cartItemRoute from "./routes/cartItemRoute.js";
 import orderRoute from "./routes/orderRoute.js";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
+import rateLimit from "express-rate-limit";
 
 //app config
 
@@ -15,9 +16,28 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT;
 
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (['https://www.deliveed.com'].indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200,
+  };
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+  });
+
 //middleware
+app.use(limiter);
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors(corsOptions));
 
 
 //DB Connection
@@ -37,7 +57,7 @@ app.get("/",(req,res)=>
     res.send("API working")
 })
 
-app.listen(port,'0.0.0.0',()=>
+app.listen(port,()=>
 {
     console.log(`server started on port  ${port}`)
 })
