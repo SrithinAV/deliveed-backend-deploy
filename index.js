3e463a90ba3612cec6269b61bc50c2d3a930d94b
Srithin A V <srithin@Srithins-MacBook-Air.local>
2024-06-25 18:25:22 +0530

    cors removed

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import cartItemRoute from "./routes/cartItemRoute.js";
 import orderRoute from "./routes/orderRoute.js";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
-import rateLimit from "express-rate-limit";
 
 //app config
 
@@ -16,28 +15,21 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT;
 
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (['https://www.deliveed.com','https://deliveed.adaptable.app','https://deliveed.netlify.app/',].indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    optionsSuccessStatus: 200,
-  };
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//       if (['https://www.deliveed.com', 'http://localhost:3000', 'http://localhost:5173', 'http://localhost:3001'].indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     optionsSuccessStatus: 200,
+//   };
 
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again after 15 minutes',
-  });
 
 //middleware
-app.use(limiter);
 app.use(express.json())
-// app.use(cors())
-app.use(cors(corsOptions));
+app.use(cors())
 
 
 //DB Connection
@@ -57,7 +49,7 @@ app.get("/",(req,res)=>
     res.send("API working")
 })
 
-app.listen(port,()=>
+app.listen(port,'0.0.0.0',()=>
 {
     console.log(`server started on port  ${port}`)
 })
